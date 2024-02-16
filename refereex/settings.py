0051eae2329fdb3bf0e62202dc3c3b8f4acd5af4
Oleksandr <sandoramix.dev@gmail.com>
2024-02-16 23:32:34 +0100

    [fix] for `static` folder; added usage example to one of templates

--- refereex/settings.py
+++ refereex/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -120,9 +119,9 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-STATIC_ROOT =   BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
